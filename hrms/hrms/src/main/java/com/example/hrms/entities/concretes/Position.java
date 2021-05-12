d01838ac5b530a9898aaea378eef31a62769287d
oguzhan35 <oguzhandemir35@gmail.com>
2021-05-12 19:39:24 +0300

    .
    12.05 position

---
+++ hrms/hrms/src/main/java/com/example/hrms/entities/concretes/Position.java
@@ -0,0 +1,52 @@
+package com.example.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="positions")
+public class Position {
+  
+	public Position() {
+		
+	}
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="position_name")
+	private String positionName;
+	
+	
+	public Position(int id, String positionName) {
+		super();
+		this.id = id;
+		this.positionName = positionName;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getPositionName() {
+		return positionName;
+	}
+
+
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+	
+	
+}
